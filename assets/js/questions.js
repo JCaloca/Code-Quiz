69a66337e17fcf83cc3df03fdaaae2c3728aa475
JCaloca <jacobcaloca1@gmail.com>
2022-09-30 15:40:04 -0700

    added question choices to javascript

---
+++ assets/js/questions.js
@@ -0,0 +1,28 @@
+let questions = [
+  {
+    title: "Q1",
+    answerChoices: ["A1", "A2", "A3", "A4"],
+    correctAnswer: "A3",
+  },
+
+  {
+    title: "Q2",
+    answerChoices: ["A1", "A2", "A3", "A4"],
+    correctAnswer: "A1",
+  },
+  {
+    title: "Q3",
+    answerChoices: ["A1", "A2", "A3", "A4"],
+    correctAnswer: "A4",
+  },
+  {
+    title: "Q4",
+    answerChoices: ["A1", "A2", "A3", "A4"],
+    correctAnswer: "A2",
+  },
+  {
+    title: "Q5",
+    answerChoices: ["A1", "A2", "A3", "A4"],
+    correctAnswer: "A1",
+  },
+];
